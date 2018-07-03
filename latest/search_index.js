e4d06e222b4b5ab3ded48db2e6a9e36de3987a2f
autodocs <autodocs>
2018-07-03 14:32:45 +0000

    build based on 3e391f5

---
+++ latest/search_index.js
@@ -0,0 +1,35 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Introduction-1",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "section",
+    "text": "Package contains solver for linear implicit dynamics problems."
+},
+
+{
+    "location": "index.html#Theory-1",
+    "page": "Introduction",
+    "title": "Theory",
+    "category": "section",
+    "text": "Theory of time integration formulations comes here."
+},
+
+{
+    "location": "index.html#Examples-1",
+    "page": "Introduction",
+    "title": "Examples",
+    "category": "section",
+    "text": "DocTestSetup = quote\n    using FEMBase\n    using LinearImplicitDynamics\nendSimple calculation examples here."
+},
+
+]}
