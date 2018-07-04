04024ae51d2be2765280f290f2e1dabb282ee9bf
autodocs <autodocs>
2018-07-04 12:53:35 +0000

    build based on 199e80c

---
+++ release-0.1/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Introduction-1",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "section",
+    "text": "Package contains dynamics solver for JuliaFEM.DocTestSetup = quote\n    using FEMBase\n    using FEMDynamics\nend"
+},
+
+]}
